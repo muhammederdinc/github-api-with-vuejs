70b8802b353d2ef14d31d40cbbb2356dddc20bab
Muhammed ERDİNÇ <erdincmuhammed@gmail.com>
2021-03-07 21:30:18 +0300

    /modules/app/store comment line made

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,13 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import app from '../modules/app/store';
+// import app from '../modules/app/store';
 import searchResults from '../modules/searchResults/store';
 
 Vue.use(Vuex);
 
 export const storeOptions = {
   modules: {
-    app,
+    // app,
     searchResults,
   },
 };
