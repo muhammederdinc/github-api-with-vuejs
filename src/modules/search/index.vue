be65cfde382bb821c78898bf35f36311299082c6
Muhammed ERDİNÇ <erdincmuhammed@gmail.com>
2021-03-14 15:00:08 +0300

    added search result detail card

--- src/modules/search/index.vue
+++ src/modules/search/index.vue
@@ -15,6 +15,39 @@ export default {
       { text: 'Wikis', action: 4 },
       { text: 'Users', action: 4 },
     ],
+    selected: [2],
+    items2: [
+      {
+        action: '15 min',
+        headline: 'Brunch this weekend?',
+        subtitle: 'Ill be in your neighborhood doing',
+        title: 'Ali Connors',
+      },
+      {
+        action: '2 hr',
+        headline: 'Summer BBQ',
+        subtitle: 'Wish I could come, but  out of town this weekend.',
+        title: 'me, Scrott, Jennifer',
+      },
+      {
+        action: '6 hr',
+        headline: 'Oui oui',
+        subtitle: 'Do you have Paris recommendations? Have you ever been?',
+        title: 'Sandra Adams',
+      },
+      {
+        action: '12 hr',
+        headline: 'Birthday gift',
+        subtitle: 'Have any ideas about what we should get Heidi for her birthday?',
+        title: 'Trevor Hansen',
+      },
+      {
+        action: '18hr',
+        headline: 'Recipe to try',
+        subtitle: 'We should eat this: Grate, Squash, Corn, and tomatillo Tacos.',
+        title: 'Britta Holt',
+      },
+    ],
   }),
 };
 </script>
@@ -48,7 +81,62 @@ export default {
       </v-col>
 
       <v-col>
-        2
+        <template>
+          <v-card
+            class="mx-2"
+          >
+            <v-list two-line>
+              <v-list-item-group
+                v-model="selected"
+                active-class="pink--text"
+              >
+                <template v-for="(item, index) in items2">
+                  <v-list-item :key="item.title">
+                    <template v-slot:default="{ active }">
+                      <v-list-item-avatar>
+                        <v-img src="https://cdn.vuetifyjs.com/images/lists/1.jpg"></v-img>
+                      </v-list-item-avatar>
+
+                      <v-list-item-content>
+                        <v-list-item-title v-text="item.title"></v-list-item-title>
+
+                        <v-list-item-subtitle
+                          class="text--primary"
+                          v-text="item.headline"
+                        ></v-list-item-subtitle>
+
+                        <v-list-item-subtitle v-text="item.subtitle"></v-list-item-subtitle>
+                      </v-list-item-content>
+
+                      <v-list-item-action>
+                        <v-list-item-action-text v-text="item.action"></v-list-item-action-text>
+
+                        <v-icon
+                          v-if="!active"
+                          color="grey lighten-1"
+                        >
+                          mdi-star-outline
+                        </v-icon>
+
+                        <v-icon
+                          v-else
+                          color="yellow darken-3"
+                        >
+                          mdi-star
+                        </v-icon>
+                      </v-list-item-action>
+                    </template>
+                  </v-list-item>
+
+                  <v-divider
+                    v-if="index < items.length - 1"
+                    :key="index"
+                  ></v-divider>
+                </template>
+              </v-list-item-group>
+            </v-list>
+          </v-card>
+        </template>
       </v-col>
     </v-row>
   </v-container>
