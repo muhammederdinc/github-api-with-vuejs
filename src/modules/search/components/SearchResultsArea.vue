6c9ece80f9de27803c3a03be7277874c39993ee7
Muhammed ERDİNÇ <erdincmuhammed@gmail.com>
2021-03-16 22:23:46 +0300

    added search result area component in search.

---
+++ src/modules/search/components/SearchResultsArea.vue
@@ -0,0 +1,72 @@
+<script>
+export default {
+  name: 'SearchResultsArea',
+  props: {
+    searchResults: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+<template>
+  <v-card class="mx-2">
+    <v-list three-line>
+      <v-list-item-group>
+        <template v-for="(item, index) in searchResults">
+          <v-list-item :key="item.title">
+            <template v-slot:default>
+              <v-list-item-avatar>
+                <v-img :src="item.owner.avatar_url" />
+              </v-list-item-avatar>
+
+              <v-list-item-content>
+                <v-list-item-title v-text="item.name" />
+
+                <v-list-item-subtitle v-text="item.description" class="text--primary" />
+
+                <v-list-item-subtitle>
+                  <v-icon small color="primary">
+                    mdi-star-outline
+                  </v-icon>
+
+                  <span class="caption">
+                    {{ item.stargazers_count }}
+                  </span>
+
+                  <template v-if="item.language">
+                    <v-icon small class="pl-3" color="orange darken-1">
+                      mdi-circle
+                    </v-icon>
+
+                    <span class="caption">
+                      {{ item.language }}
+                    </span>
+                  </template>
+
+                  <span v-if="item.license" class="caption pl-3">
+                    {{ item.license.name }}
+                  </span>
+
+                  <span class="caption pl-3">
+                    Updated on {{ new Date(item.updated_at).getFullYear() }}
+                  </span>
+                </v-list-item-subtitle>
+              </v-list-item-content>
+
+              <v-list-item-action>
+                <v-list-item-action-text v-text="item.created_at" />
+              </v-list-item-action>
+            </template>
+          </v-list-item>
+
+          <v-divider
+            v-if="index < searchResults.length - 1"
+            :key="index"
+          />
+        </template>
+      </v-list-item-group>
+    </v-list>
+  </v-card>
+</template>
--- src/modules/search/index.vue
+++ src/modules/search/index.vue
@@ -1,11 +1,13 @@
 <script>
 import { mapState } from 'vuex';
 import SearchTypesCard from './components/SearchTypesCard';
+import SearchResultsArea from './components/SearchResultsArea';
 
 export default {
   name: 'Search',
   components: {
     SearchTypesCard,
+    SearchResultsArea,
   },
   computed: {
     ...mapState('search', ['searchResult', 'searchTypes']),
@@ -23,60 +25,9 @@ export default {
       </v-col>
 
       <v-col cols="10">
-        <template>
-          <v-card class="mx-2">
-            <v-list three-line>
-              <v-list-item-group>
-                <template v-for="(item, index) in searchResult.items">
-                  <v-list-item :key="item.title">
-                    <template v-slot:default>
-                      <v-list-item-avatar>
-                        <v-img :src="item.owner.avatar_url" />
-                      </v-list-item-avatar>
-
-                      <v-list-item-content>
-                        <v-list-item-title v-text="item.name" />
-
-                        <v-list-item-subtitle v-text="item.description" class="text--primary" />
-
-                        <v-list-item-subtitle>
-                          <v-icon small color="primary">mdi-star-outline</v-icon>
-                          <span class="caption">
-                            {{ item.stargazers_count }}
-                          </span>
-
-                          <template v-if="item.language">
-                            <v-icon small class="pl-3" color="orange darken-1">mdi-circle</v-icon>
-                            <span class="caption">
-                              {{ item.language }}
-                            </span>
-                          </template>
-
-                          <span v-if="item.license" class="caption pl-3">
-                            {{ item.license.name }}
-                          </span>
-
-                          <span class="caption pl-3">
-                            Updated on {{ new Date(item.updated_at).getFullYear() }}
-                          </span>
-                        </v-list-item-subtitle>
-                      </v-list-item-content>
-
-                      <v-list-item-action>
-                        <v-list-item-action-text v-text="item.created_at" />
-                      </v-list-item-action>
-                    </template>
-                  </v-list-item>
-
-                  <v-divider
-                    v-if="index < searchResult.items.length - 1"
-                    :key="index"
-                  />
-                </template>
-              </v-list-item-group>
-            </v-list>
-          </v-card>
-        </template>
+        <search-results-area
+          :search-results="searchResult.items || []"
+        />
       </v-col>
     </v-row>
   </v-container>
