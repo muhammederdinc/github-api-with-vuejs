8c24f92e1b8cf89d4755ad460245ff69daf953d8
Muhammed ERDİNÇ <erdincmuhammed@gmail.com>
2021-03-20 22:30:11 +0300

    added fetch user request.

--- src/modules/user/store/actions.js
+++ src/modules/user/store/actions.js
@@ -1,3 +1,10 @@
-export default {
+import axios from 'axios';
 
+export default { /* eslint-disable*/
+  fetchUserDetail({ commit }, username) {
+    axios.get(`users/${username}`)
+      .then(({ data }) => {
+        commit('setUser', data);
+      });
+  },
 };
--- src/modules/user/store/index.js
+++ src/modules/user/store/index.js
@@ -1,6 +1,10 @@
 import actions from './actions';
+import mutations from './mutations';
+import state from './state';
 
 export default {
   actions,
+  mutations,
+  state,
   namespaced: true,
 };
