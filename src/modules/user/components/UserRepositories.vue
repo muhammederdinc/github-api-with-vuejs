e14594106d1153a6902a157754e01507c8779616
Muhammed ERDİNÇ <erdincmuhammed@gmail.com>
2021-04-17 18:00:57 +0300

    UserRepositories component has been created.

---
+++ src/modules/user/components/UserRepositories.vue
@@ -0,0 +1,87 @@
+<script>
+export default {
+  name: 'UserRepositories',
+  props: {
+    repositories: {
+      type: Array,
+      reuqired: true,
+    },
+  },
+  methods: {
+    getDescription(item) {
+      const { description } = item;
+      const isDescriptionLong = description && description.length > 350;
+
+      return isDescriptionLong ? `${description.substring(0, 349)}...` : description;
+    },
+  },
+};
+</script>
+
+<template>
+  <v-data-iterator
+    :items="repositories"
+  >
+    <template v-slot:default="{ items }">
+      <v-row>
+        <v-col v-for="item in items" :key="item.id" cols="12">
+          <v-card outlined>
+            <v-card-title>
+              <v-row>
+                <v-col cols="9">
+                  {{ item.name }}
+                </v-col>
+
+                <v-col cols="3" class="text-right caption pa-0 ma-0">
+                  Created: {{ item.created_at }}
+
+                  <br />
+
+                  <v-chip x-small class="mr-1" outlined>
+                    <v-icon color="primary" x-small left>mdi-star</v-icon>
+                    {{ item.stargazers_count }}
+                  </v-chip>
+
+                  <v-chip x-small class="mr-1" outlined>
+                    <v-icon color="deep-purple accent-4" x-small left>mdi-eye</v-icon>
+                    {{ item.watchers_count }}
+                  </v-chip>
+                </v-col>
+              </v-row>
+            </v-card-title>
+
+            <v-card-text>
+              <v-row>
+                <v-col cols="12">
+                  {{ getDescription(item) }}
+                </v-col>
+              </v-row>
+            </v-card-text>
+
+            <v-card-actions class="caption py-0 my-0">
+              <v-chip v-if="item.language" x-small class="ma-1" outlined>
+                <v-icon color="primary" x-small left>mdi-circle</v-icon>
+                {{ item.language}}
+              </v-chip>
+
+              <v-chip x-small class="ma-1" outlined>
+                <v-icon color="primary" x-small left>mdi-source-fork</v-icon>
+                {{ item.forks_count }}
+              </v-chip>
+
+              <v-chip v-if="item.license" x-small class="ma-1" outlined>
+                <v-icon color="primary" x-small left>mdi-anchor</v-icon>
+                {{ item.license.name }}
+              </v-chip>
+
+              <v-chip x-small class="ma-1" outlined>
+                <v-icon color="primary" x-small left>mdi-calendar</v-icon>
+                Updated on: {{ item.pushed_at }}
+              </v-chip>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </template>
+  </v-data-iterator>
+</template>
--- src/modules/user/index.vue
+++ src/modules/user/index.vue
@@ -1,8 +1,12 @@
 <script>
 import { mapActions, mapState } from 'vuex';
+import UserRepositories from './components/UserRepositories';
 
 export default {
   name: 'User',
+  components: {
+    UserRepositories,
+  },
   data: () => ({
     rating: 4.3,
     username: '',
@@ -148,74 +152,8 @@ export default {
         </div>
       </v-col>
 
-      <v-col cols="9" sm="12" lg="9" style="border: 1px solid">
-        <v-data-iterator
-          :items="repos"
-          items-per-page.sync="4"
-        >
-          <template v-slot:default="{ items }">
-            <v-row>
-              <v-col v-for="item in items" :key="item.id" cols="12">
-                <v-card outlined>
-                  <v-card-title>
-                    <v-row>
-                      <v-col cols="9">
-                        {{ item.name }}
-                      </v-col>
-
-                      <v-col cols="3" class="text-right caption pa-0 ma-0">
-                        Created: {{ item.created_at }}
-
-                        <br />
-
-                        <v-chip x-small class="mr-1" outlined>
-                          <v-icon color="primary" x-small left>mdi-star</v-icon>
-                          {{ item.stargazers_count }}
-                        </v-chip>
-
-                        <v-chip x-small class="mr-1" outlined>
-                          <v-icon color="deep-purple accent-4" x-small left>mdi-eye</v-icon>
-                          {{ item.watchers_count }}
-                        </v-chip>
-                      </v-col>
-                    </v-row>
-                  </v-card-title>
-
-                  <v-card-text>
-                    <v-row>
-                      <v-col cols="12">
-                        {{ item.description && item.description.length > 350 ?
-                        `${item.description.substring(0, 349)}...` : item.description }}
-                      </v-col>
-                    </v-row>
-                  </v-card-text>
-
-                  <v-card-actions class="caption py-0 my-0">
-                    <v-chip v-if="item.language" x-small class="ma-1" outlined>
-                      <v-icon color="primary" x-small left>mdi-circle</v-icon>
-                      {{ item.language}}
-                    </v-chip>
-
-                    <v-chip x-small class="ma-1" outlined>
-                      <v-icon color="primary" x-small left>mdi-source-fork</v-icon>
-                      {{ item.forks_count }}
-                    </v-chip>
-
-                    <v-chip v-if="item.license" x-small class="ma-1" outlined>
-                      <v-icon color="primary" x-small left>mdi-anchor</v-icon>
-                      {{ item.license.name }}
-                    </v-chip>
-
-                    <v-chip x-small class="ma-1" outlined>
-                      <v-icon color="primary" x-small left>mdi-calendar</v-icon>
-                      Updated on: {{ item.pushed_at }}
-                    </v-chip>
-                  </v-card-actions>
-                </v-card>
-              </v-col>
-            </v-row>
-          </template>
-        </v-data-iterator>
+      <v-col cols="9" sm="12" lg="9">
+        <user-repositories :repositories="repos" />
       </v-col>
     </v-row>
   </v-container>
