c196c74813fe2c571b3109aa14b8fad979c9495c
Muhammed ERDİNÇ <erdincmuhammed@gmail.com>
2021-04-17 18:06:46 +0300

    UserInformation component has been created.

--- src/modules/user/index.vue
+++ src/modules/user/index.vue
@@ -1,11 +1,13 @@
 <script>
 import { mapActions, mapState } from 'vuex';
 import UserRepositories from './components/UserRepositories';
+import UserInformation from './components/UserInformation';
 
 export default {
   name: 'User',
   components: {
     UserRepositories,
+    UserInformation,
   },
   data: () => ({
     rating: 4.3,
@@ -39,117 +41,7 @@ export default {
   <v-container fluid>
     <v-row>
       <v-col cols="3" sm="12" lg="3">
-        <div>
-          <v-avatar
-            color="indigo"
-            size="240"
-          >
-            <img
-              :src="user.avatar_url"
-              alt="John"
-            >
-          </v-avatar>
-        </div>
-
-        <div
-          v-text="user.name"
-          class="px-3 pt-3 headline"
-        />
-
-        <div class="px-3 body-1">
-          {{ `@${user.login}` }}
-        </div>
-
-        <div
-          v-text="user.bio"
-          class="px-3 pt-3 caption"
-        />
-
-        <div class="px-3 pt-3 caption">
-          <v-icon small class="pr-2">
-            mdi-account-multiple
-          </v-icon>
-
-          <v-chip
-            class="user-text-hover"
-            x-small outlined
-          >
-            Follewers: <span v-text="user.followers" class="font-weight-bold caption" />
-          </v-chip>
-
-          <v-icon class="mb-1" small>
-            mdi-circle-small
-          </v-icon>
-
-          <v-chip
-            class="user-text-hover"
-            x-small outlined
-          >
-            Following: <span v-text="user.following" class="font-weight-bold caption" />
-          </v-chip>
-
-          <v-chip
-            class="ml-3"
-            x-small outlined
-          >
-            Created: {{ new Date(user.created_at).getFullYear() }}
-          </v-chip>
-        </div>
-
-        <div class="pt-2 caption text-center">
-          <template v-if="user.company">
-            <v-icon
-              class="mb-1"
-              color="primary"
-              small
-            >
-              mdi-domain
-            </v-icon>
-
-            {{ user.company }}
-          </template>
-
-          <template v-if="user.location">
-            <v-icon
-              class="mb-1 pl-2"
-              color="primary"
-              small
-            >
-              mdi-map-marker
-            </v-icon>
-
-            {{ user.location }}
-          </template>
-        </div>
-
-        <div class="pt-2 caption text-truncate">
-          <template v-if="user.blog">
-            <v-icon
-              color="primary"
-              small
-            >
-              mdi-link
-            </v-icon>
-
-            <a
-              v-text="user.blog"
-              :href="user.blog"
-              target="_blank"
-            />
-          </template>
-
-          <template v-if="user.twitter_username">
-            <v-icon
-              class="pl-2"
-              color="primary"
-              small
-            >
-              mdi-twitter
-            </v-icon>
-
-            {{ user.twitter_username }}
-          </template>
-        </div>
+        <user-information :user="user" />
       </v-col>
 
       <v-col cols="9" sm="12" lg="9">
