7c39cf1bea441a545735b0c10b8145a689f54453
Muhammed ERDİNÇ <erdincmuhammed@gmail.com>
2021-04-18 14:39:15 +0300

    ContributorsDialog component has been created.

---
+++ src/modules/user/components/ContributorsDialog.vue
@@ -0,0 +1,43 @@
+<script>
+export default {
+  name: 'ContributorsDialog',
+  data() {
+    return {
+      isDialogVisible: false,
+    };
+  },
+  mounted() {
+    this.isDialogVisible = true;
+  },
+};
+</script>
+
+<template>
+  <v-dialog
+    v-model="isDialogVisible"
+    width="500"
+    persistent
+  >
+    <v-card>
+      <v-card-title>
+        Contributors
+      </v-card-title>
+
+      <v-card-text>
+        ds
+      </v-card-text>
+
+      <v-divider />
+
+      <v-card-actions>
+        <v-spacer />
+        <v-btn
+          v-text="'Close'"
+          color="error"
+          text small
+          @click="$emit('close')"
+        />
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
--- src/modules/user/components/UserRepositories.vue
+++ src/modules/user/components/UserRepositories.vue
@@ -1,11 +1,21 @@
 <script>
+import ContributorsDialog from './ContributorsDialog';
+
 export default {
   name: 'UserRepositories',
+  components: {
+    ContributorsDialog,
+  },
   props: {
     repositories: {
       type: Array,
       reuqired: true,
     },
+  },
+  data() {
+    return {
+      isContributorsDialogVisible: false,
+    };
   },
   methods: {
     getDescription(item) {
@@ -19,69 +29,89 @@ export default {
 </script>
 
 <template>
-  <v-data-iterator
-    :items="repositories"
-  >
-    <template v-slot:default="{ items }">
-      <v-row>
-        <v-col v-for="item in items" :key="item.id" cols="12">
-          <v-card outlined>
-            <v-card-title>
-              <v-row>
-                <v-col cols="9">
-                  {{ item.name }}
-                </v-col>
+  <div>
+    <v-data-iterator
+      :items="repositories"
+    >
+      <template v-slot:default="{ items }">
+        <v-row>
+          <v-col v-for="item in items" :key="item.id" cols="12">
+            <v-card outlined>
+              <v-card-title>
+                <v-row>
+                  <v-col cols="9">
+                    {{ item.name }}
 
-                <v-col cols="3" class="text-right caption pa-0 ma-0">
-                  Created: {{ item.created_at }}
+                    <v-chip v-if="item.fork" x-small outlined>
+                      Forked
+                    </v-chip>
+                  </v-col>
 
-                  <br />
+                  <v-col cols="3" class="text-right caption pa-0 ma-0">
+                    Created: {{ item.created_at }}
 
-                  <v-chip x-small class="mr-1" outlined>
-                    <v-icon color="primary" x-small left>mdi-star</v-icon>
-                    {{ item.stargazers_count }}
-                  </v-chip>
+                    <br />
 
-                  <v-chip x-small class="mr-1" outlined>
-                    <v-icon color="deep-purple accent-4" x-small left>mdi-eye</v-icon>
-                    {{ item.watchers_count }}
-                  </v-chip>
-                </v-col>
-              </v-row>
-            </v-card-title>
+                    <v-chip x-small class="mr-1" outlined>
+                      <v-icon color="primary" x-small left>mdi-star</v-icon>
+                      {{ item.stargazers_count }}
+                    </v-chip>
 
-            <v-card-text>
-              <v-row>
-                <v-col cols="12">
-                  {{ getDescription(item) }}
-                </v-col>
-              </v-row>
-            </v-card-text>
+                    <v-chip x-small class="mr-1" outlined>
+                      <v-icon color="deep-purple accent-4" x-small left>mdi-eye</v-icon>
+                      {{ item.watchers_count }}
+                    </v-chip>
+                  </v-col>
+                </v-row>
+              </v-card-title>
 
-            <v-card-actions class="caption py-0 my-0">
-              <v-chip v-if="item.language" x-small class="ma-1" outlined>
-                <v-icon color="primary" x-small left>mdi-circle</v-icon>
-                {{ item.language}}
-              </v-chip>
+              <v-card-text>
+                <v-row>
+                  <v-col cols="12">
+                    {{ getDescription(item) }}
+                  </v-col>
+                </v-row>
+              </v-card-text>
 
-              <v-chip x-small class="ma-1" outlined>
-                <v-icon color="primary" x-small left>mdi-source-fork</v-icon>
-                {{ item.forks_count }}
-              </v-chip>
+              <v-card-actions class="caption py-0 my-0">
+                <v-chip v-if="item.language" x-small class="ma-1" outlined>
+                  <v-icon color="primary" x-small left>mdi-circle</v-icon>
+                  {{ item.language}}
+                </v-chip>
 
-              <v-chip v-if="item.license" x-small class="ma-1" outlined>
-                <v-icon color="primary" x-small left>mdi-anchor</v-icon>
-                {{ item.license.name }}
-              </v-chip>
+                <v-chip x-small class="ma-1" outlined>
+                  <v-icon color="primary" x-small left>mdi-source-fork</v-icon>
+                  {{ item.forks_count }}
+                </v-chip>
 
-              <v-chip x-small class="ma-1" outlined>
-                <v-icon color="primary" x-small left>mdi-calendar</v-icon>
-                Updated on: {{ item.pushed_at }}
-              </v-chip>
-            </v-card-actions>
-          </v-card>
-        </v-col>
-      </v-row>
-    </template>
-  </v-data-iterator>
+                <v-chip v-if="item.license" x-small class="ma-1" outlined>
+                  <v-icon color="primary" x-small left>mdi-anchor</v-icon>
+                  {{ item.license.name }}
+                </v-chip>
+
+                <v-chip x-small class="ma-1" outlined>
+                  <v-icon color="primary" x-small left>mdi-calendar</v-icon>
+                  Updated on: {{ item.pushed_at }}
+                </v-chip>
+
+                <v-spacer />
+
+                <a
+                  v-text="`contributors`"
+                  class="text-decoration-none"
+                  @click="isContributorsDialogVisible = true"
+                />
+
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </template>
+    </v-data-iterator>
+
+    <contributors-dialog
+      v-if="isContributorsDialogVisible"
+      @close="isContributorsDialogVisible = false"
+    />
+  </div>
 </template>
