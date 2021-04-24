2f52bdb7035e41def7739480aa6c80220c746aa7
Muhammed ERDİNÇ <erdincmuhammed@gmail.com>
2021-04-24 15:25:57 +0300

    dateFormat mixin has been created.

---
+++ src/mixins/dateFormat.js
@@ -0,0 +1,14 @@
+export default {
+  name: 'dateFormat',
+  methods: {
+    $_getFormattedDate(date, dateOptions = null) {
+      const event = new Date(date);
+      let options = null;
+
+      if (dateOptions) options = dateOptions;
+      else options = { year: 'numeric', month: 'numeric', day: 'numeric' };
+
+      return event.toLocaleString('en-GB', options);
+    },
+  },
+};
