c70c71bc99decc9defcddadd2c061851edd49d6e
Muhammed ERDİNÇ <erdincmuhammed@gmail.com>
2021-04-24 15:39:51 +0300

    UserRepositoryCard has been created.

--- src/modules/user/components/UserRepositories.vue
+++ src/modules/user/components/UserRepositories.vue
@@ -1,13 +1,13 @@
 <script>
-import dateFormat from '@/mixins/dateFormat';
 import ContributorsDialog from './ContributorsDialog';
+import UserRepositoryCard from './UserRepositoryCard';
 
 export default {
   name: 'UserRepositories',
   components: {
     ContributorsDialog,
+    UserRepositoryCard,
   },
-  mixins: [dateFormat],
   props: {
     repositories: {
       type: Array,
@@ -21,12 +21,6 @@ export default {
     };
   },
   methods: {
-    getDescription(item) {
-      const { description } = item;
-      const isDescriptionLong = description && description.length > 350;
-
-      return isDescriptionLong ? `${description.substring(0, 349)}...` : description;
-    },
     openContributorsDialog(url) {
       this.isContributorsDialogVisible = true;
       this.contributorsUrl = url;
@@ -43,85 +37,10 @@ export default {
       <template v-slot:default="{ items }">
         <v-row>
           <v-col v-for="item in items" :key="item.id" cols="12">
-            <v-card outlined>
-              <v-card-title>
-                <v-row>
-                  <v-col cols="9">
-                    {{ item.name }}
-
-                    <v-chip v-if="item.fork" x-small outlined>
-                      Forked
-                    </v-chip>
-                  </v-col>
-
-                  <v-col cols="3" class="text-right caption pa-0 ma-0">
-                    Created: {{ $_getFormattedDate(item.created_at) }}
-
-                    <br>
-
-                    <v-chip v-if="item.stargazers_count" x-small class="mr-1" outlined>
-                      <v-icon color="primary" x-small left>
-                        mdi-star
-                      </v-icon>
-                      {{ item.stargazers_count }}
-                    </v-chip>
-
-                    <v-chip v-if="item.watchers_count" x-small class="mr-1" outlined>
-                      <v-icon color="deep-purple accent-4" x-small left>
-                        mdi-eye
-                      </v-icon>
-                      {{ item.watchers_count }}
-                    </v-chip>
-                  </v-col>
-                </v-row>
-              </v-card-title>
-
-              <v-card-text>
-                <v-row>
-                  <v-col cols="12">
-                    {{ getDescription(item) }}
-                  </v-col>
-                </v-row>
-              </v-card-text>
-
-              <v-card-actions class="caption py-0 my-0">
-                <v-chip v-if="item.language" x-small class="ma-1" outlined>
-                  <v-icon color="primary" x-small left>
-                    mdi-circle
-                  </v-icon>
-                  {{ item.language }}
-                </v-chip>
-
-                <v-chip v-if="item.forks_count" x-small class="ma-1" outlined>
-                  <v-icon color="primary" x-small left>
-                    mdi-source-fork
-                  </v-icon>
-                  {{ item.forks_count }}
-                </v-chip>
-
-                <v-chip v-if="item.license" x-small class="ma-1" outlined>
-                  <v-icon color="primary" x-small left>
-                    mdi-anchor
-                  </v-icon>
-                  {{ item.license.name }}
-                </v-chip>
-
-                <v-chip x-small class="ma-1" outlined>
-                  <v-icon color="primary" x-small left>
-                    mdi-calendar
-                  </v-icon>
-                  Updated on: {{ item.pushed_at }}
-                </v-chip>
-
-                <v-spacer />
-
-                <a
-                  v-text="`contributors`"
-                  class="text-decoration-none"
-                  @click="openContributorsDialog(item.contributors_url)"
-                />
-              </v-card-actions>
-            </v-card>
+            <user-repository-card
+              :item="item"
+              @showContributors="openContributorsDialog"
+            />
           </v-col>
         </v-row>
       </template>
---
+++ src/modules/user/components/UserRepositoryCard.vue
@@ -0,0 +1,107 @@
+<script>
+import dateFormat from '@/mixins/dateFormat';
+
+export default {
+  name: 'UserRepositoryCard',
+  mixins: [dateFormat],
+  props: {
+    item: {
+      type: Object,
+      required: true,
+    },
+  },
+  methods: {
+    getDescription(item) {
+      const { description } = item;
+      const isDescriptionLong = description && description.length > 350;
+
+      return isDescriptionLong ? `${description.substring(0, 349)}...` : description;
+    },
+    showContributors(url) {
+      this.$emit('showContributors', url);
+    },
+  },
+};
+</script>
+
+<template>
+  <v-card outlined>
+    <v-card-title>
+      <v-row>
+        <v-col cols="9">
+          {{ item.name }}
+
+          <v-chip v-if="item.fork" x-small outlined>
+            Forked
+          </v-chip>
+        </v-col>
+
+        <v-col cols="3" class="text-right caption pa-0 ma-0">
+          Created: {{ $_getFormattedDate(item.created_at) }}
+
+          <br>
+
+          <v-chip v-if="item.stargazers_count" x-small class="mr-1" outlined>
+            <v-icon color="primary" x-small left>
+              mdi-star
+            </v-icon>
+            {{ item.stargazers_count }}
+          </v-chip>
+
+          <v-chip v-if="item.watchers_count" x-small class="mr-1" outlined>
+            <v-icon color="deep-purple accent-4" x-small left>
+              mdi-eye
+            </v-icon>
+            {{ item.watchers_count }}
+          </v-chip>
+        </v-col>
+      </v-row>
+    </v-card-title>
+
+    <v-card-text>
+      <v-row>
+        <v-col cols="12">
+          {{ getDescription(item) }}
+        </v-col>
+      </v-row>
+    </v-card-text>
+
+    <v-card-actions class="caption py-0 my-0">
+      <v-chip v-if="item.language" x-small class="ma-1" outlined>
+        <v-icon color="primary" x-small left>
+          mdi-circle
+        </v-icon>
+        {{ item.language }}
+      </v-chip>
+
+      <v-chip v-if="item.forks_count" x-small class="ma-1" outlined>
+        <v-icon color="primary" x-small left>
+          mdi-source-fork
+        </v-icon>
+        {{ item.forks_count }}
+      </v-chip>
+
+      <v-chip v-if="item.license" x-small class="ma-1" outlined>
+        <v-icon color="primary" x-small left>
+          mdi-anchor
+        </v-icon>
+        {{ item.license.name }}
+      </v-chip>
+
+      <v-chip x-small class="ma-1" outlined>
+        <v-icon color="primary" x-small left>
+          mdi-calendar
+        </v-icon>
+        Updated on: {{ item.pushed_at }}
+      </v-chip>
+
+      <v-spacer />
+
+      <a
+        v-text="`contributors`"
+        class="text-decoration-none"
+        @click="showContributors(item.contributors_url)"
+      />
+    </v-card-actions>
+  </v-card>
+</template>
