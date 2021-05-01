db786fe13c2434baf55295e7053e37df5b51284c
Muhammed ERDİNÇ <erdincmuhammed@gmail.com>
2021-05-01 15:35:35 +0300

    code refactoring in UserInformation

--- src/modules/user/components/UserInformation.vue
+++ src/modules/user/components/UserInformation.vue
@@ -11,117 +11,106 @@ export default {
 </script>
 
 <template>
-  <div>
-    <div>
-      <v-avatar
-        color="indigo"
-        size="240"
-      >
-        <img
-          :src="user.avatar_url"
-          alt="John"
-        >
-      </v-avatar>
-    </div>
+  <v-container>
+    <v-row no-gutters>
+      <v-col cols="12">
+        <v-avatar size="240">
+          <img :src="user.avatar_url" :alt="user.name">
+        </v-avatar>
+      </v-col>
 
-    <div
-      v-text="user.name"
-      class="px-3 pt-3 headline"
-    />
+      <v-col v-text="user.name" class="pt-3 headline" cols="12" />
 
-    <div class="px-3 body-1">
-      {{ `@${user.login}` }}
-    </div>
+      <v-col v-text="`@${user.login}`" class="body-1" cols="12" />
 
-    <div
-      v-text="user.bio"
-      class="px-3 pt-3 caption"
-    />
+      <v-col v-if="user.bio" v-text="user.bio" class="pt-3 caption" cols="12" />
 
-    <div class="px-3 pt-3 caption">
-      <v-icon small class="pr-2">
-        mdi-account-multiple
-      </v-icon>
-
-      <v-chip
-        class="user-text-hover"
-        x-small outlined
-      >
-        Follewers: <span v-text="user.followers" class="font-weight-bold caption" />
-      </v-chip>
-
-      <v-icon class="mb-1" small>
-        mdi-circle-small
-      </v-icon>
-
-      <v-chip
-        class="user-text-hover"
-        x-small outlined
-      >
-        Following: <span v-text="user.following" class="font-weight-bold caption" />
-      </v-chip>
-
-      <v-chip
-        class="ml-3"
-        x-small outlined
-      >
-        Created: {{ new Date(user.created_at).getFullYear() }}
-      </v-chip>
-    </div>
-
-    <div class="pt-2 caption text-center">
-      <template v-if="user.company">
-        <v-icon
-          class="mb-1"
-          color="primary"
-          small
-        >
-          mdi-domain
+      <v-col class="pt-3 caption" cols="12">
+        <v-icon small class="pr-2">
+          mdi-account-multiple
         </v-icon>
 
-        {{ user.company }}
-      </template>
-
-      <template v-if="user.location">
-        <v-icon
-          class="mb-1 pl-2"
-          color="primary"
-          small
+        <v-chip
+          class="user-text-hover"
+          x-small outlined
         >
-          mdi-map-marker
+          Follewers: <span v-text="user.followers" />
+        </v-chip>
+
+        <v-icon class="mb-1" small>
+          mdi-circle-small
         </v-icon>
 
-        {{ user.location }}
-      </template>
-    </div>
-
-    <div class="pt-2 caption text-truncate">
-      <template v-if="user.blog">
-        <v-icon
-          color="primary"
-          small
+        <v-chip
+          class="user-text-hover"
+          x-small outlined
         >
-          mdi-link
-        </v-icon>
+          Following: <span v-text="user.following" />
+        </v-chip>
+      </v-col>
 
-        <a
-          v-text="user.blog"
-          :href="user.blog"
-          target="_blank"
-        />
-      </template>
+      <v-col v-if="user.created_at" cols="12">
+        <v-icon small class="pr-2">mdi-calendar-check</v-icon>
 
-      <template v-if="user.twitter_username">
-        <v-icon
-          class="pl-2"
-          color="primary"
-          small
-        >
-          mdi-twitter
-        </v-icon>
+        <v-chip x-small outlined>
+          Created: {{ new Date(user.created_at).getFullYear() }}
+        </v-chip>
+      </v-col>
 
-        {{ user.twitter_username }}
-      </template>
-    </div>
-  </div>
+      <v-col class="pt-2 caption" cols="12">
+        <template v-if="user.company">
+          <v-icon
+            class="mb-1"
+            color="primary"
+            small
+          >
+            mdi-domain
+          </v-icon>
+
+          {{ user.company }}
+        </template>
+
+        <template v-if="user.location">
+          <v-icon
+            class="mb-1 pl-2"
+            color="primary"
+            small
+          >
+            mdi-map-marker
+          </v-icon>
+
+          {{ user.location }}
+        </template>
+      </v-col>
+
+      <v-col class="pt-2 caption text-truncate">
+        <template v-if="user.blog">
+          <v-icon
+            color="primary"
+            small
+          >
+            mdi-link
+          </v-icon>
+
+          <a
+            v-text="user.blog"
+            :href="user.blog"
+            target="_blank"
+          />
+        </template>
+
+        <template v-if="user.twitter_username">
+          <v-icon
+            class="pl-2"
+            color="primary"
+            small
+          >
+            mdi-twitter
+          </v-icon>
+
+          {{ user.twitter_username }}
+        </template>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
