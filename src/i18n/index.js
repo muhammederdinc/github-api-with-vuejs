e86e5d8aca35bad513be2f0048ed05d2f059a394
Muhammed ERDİNÇ <erdincmuhammed@gmail.com>
2021-05-09 18:16:51 +0300

    tr.lang.json has been added for turkish language support.

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueI18n from 'vue-i18n';
 import en from './languages/en.lang.json';
+import tr from './languages/tr.lang.json';
 
 Vue.use(VueI18n);
 
@@ -8,5 +9,6 @@ export default new VueI18n({
   locale: 'en',
   messages: {
     en,
+    tr,
   },
 });
