5b2d874d5aa7d5e1112f87f7ac0b8cd6b61340cf
Muhammed ERDİNÇ <erdincmuhammed@gmail.com>
2021-05-09 18:05:45 +0300

    i18n localization has been added.

--- src/main.js
+++ src/main.js
@@ -4,12 +4,14 @@ import App from './modules/app';
 import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
+import i18n from './i18n';
 
 Vue.config.productionTip = false;
 
 axios.defaults.baseURL = 'https://api.github.com/';
 
 new Vue({
+  i18n,
   router,
   store,
   vuetify,
