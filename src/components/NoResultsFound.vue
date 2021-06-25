f17204886c255428b04c3a850404b074ef8bdb0f
Muhammed ERDİNÇ <erdincmuhammed@gmail.com>
2021-06-25 22:21:15 +0300

    no results found component has been created

---
+++ src/components/NoResultsFound.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+  name: 'NoResultsFound',
+};
+</script>
+
+<template>
+  <v-row
+    class="no-search-results-found"
+    justify="center"
+    align="center"
+  >
+    <v-col
+      cols="12"
+      class="d-flex justify-center align-center"
+    >
+      <v-img
+        max-width="250"
+        src="@/assets/images/noSearchResultsFound.svg"
+      />
+    </v-col>
+  </v-row>
+</template>
+
+<style lang="scss" scoped>
+  .no-search-results-found {
+    min-height: 65vh;
+  }
+</style>
